0f14b3d5dceaded1a1f2086e504f41cc291f408a
fahimaAzizi <118080917+fahimaAzizi@users.noreply.github.com>
2024-04-06 02:17:56 +0500

    Update py3.py

--- py3.py
+++ py3.py
@@ -35,9 +35,15 @@ def main():
 
         elif choice == '3':
             print('\nTotal Expenses: ', total_expenses(expenses))
+
         elif choice == '4':
             category = input('Enter category to filter: ')
             print(f'\nExpenses for {category}:')
             expenses_from_category = filter_expenses_by_category(expenses, category)
             print_expenses(expenses_from_category)
-        
+
+        elif choice == '5':
+            print('Exiting the program.')
+            break
+
+main()
