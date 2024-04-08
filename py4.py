7d2ab2d4f07b83d990a5c474d88e470b784f55ac
fahimaAzizi <118080917+fahimaAzizi@users.noreply.github.com>
2024-04-09 02:44:50 +0500

    Update py4.py

--- py4.py
+++ py4.py
@@ -3,3 +3,7 @@ def convert_to_snake_case(pascal_or_camel_cased_string):
     for char in pascal_or_camel_cased_string:
         if char.isupper():
             converted_character = '_' + char.lower()
+            snake_cased_char_list.append(converted_character)
+        else:
+            snake_cased_char_list.append(char)
+
