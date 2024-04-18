24d2981d06027157b0ab8ba19e67633f827ec476
fahimaAzizi <118080917+fahimaAzizi@users.noreply.github.com>
2024-04-18 22:29:18 +0500

    Update py5.py

--- py5.py
+++ py5.py
@@ -1,10 +1,10 @@
-
 import re
 import secrets
 import string
 
 
-def generate_password(length, nums, special_chars, uppercase, lowercase):
+def generate_password(length=16, nums=1, special_chars=1, uppercase=1, lowercase=1):
+
     # Define the possible characters for the password
     letters = string.ascii_letters
     digits = string.digits
@@ -18,17 +18,23 @@ def generate_password(length, nums, special_chars, uppercase, lowercase):
         # Generate password
         for _ in range(length):
             password += secrets.choice(all_characters)
+        
         constraints = [
             (nums, r'\d'),
-            (lowercase, r'[^a-z]'),
-            (uppercase, r'[^A-Z]'),
-            (special_chars, r'[^a-zA-Z0-9]')
-        ]        
+            (special_chars, fr'[{symbols}]'),
+            (uppercase, r'[A-Z]'),
+            (lowercase, r'[a-z]')
+        ]
+
+        # Check constraints        
+        if all(
+            constraint <= len(re.findall(pattern, password))
+            for constraint, pattern in constraints
+        ):
+            break
+    
     return password
     
-# new_password = generate_password(8)
-# print(new_password)
-
-pattern = r'\.'
-quote = 'Not all those who wander are lost.'
-# print(re.findall(pattern, quote))
+if __name__ == '__main__':
+   new_password = generate_password()
+   print('Generated password:', new_password)
