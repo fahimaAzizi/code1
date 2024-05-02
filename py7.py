f6e67ab87ee5b5ca296a3d12cb219dff01cfbf1b
fahimaAzizi <118080917+fahimaAzizi@users.noreply.github.com>
2024-05-02 17:10:10 +0500

    Update py7.py

--- py7.py
+++ py7.py
@@ -5,6 +5,21 @@ rods = {
     'B': [],
     'C': []
 }
+def make_allowed_move(rod1, rod2):
+    forward = False
+    if not rods[target]:
+        forward = True
+    elif rods[source] and rods[source][-1] < rods[target][-1]:
+        forward = True              
+    if forward:
+        print(f'Moving disk {rods[source][-1]} from {source} to {target}')
+        rods[target].append(rods[source].pop())
+    else:
+        print(f'Moving disk {rods[target][-1]} from {target} to {source}')
+        rods[source].append(rods[target].pop())
+    
+    # display our progress
+    print(rods)
 
 def move(n, source, auxiliary, target):
     # display starting configuration
@@ -12,18 +27,7 @@ def move(n, source, auxiliary, target):
     for i in range(number_of_moves):
         remainder = (i + 1) % 3
         if remainder == 1:
-            print(f'Move {i + 1} allowed between {source} and {target}')
-            forward = False
-            if not rods[target]:
-                forward = True
-            elif rods[source] and rods[source][-1] < rods[target][-1]:
-                forward = True
-            if forward:
-                print(f'Moving disk {rods[source][-1]} from {source} to {target}')
-                rods[target].append(rods[source].pop())
-            else:
-                print(f'Moving disk {rods[target][-1]} from {target} to {source}')
-                rods[source].append(rods[target].pop())
+            print(f'Move {i + 1} allowed between {source} and {target}')            
         elif remainder == 2:
             print(f'Move {i + 1} allowed between {source} and {auxiliary}')
         elif remainder == 0:
