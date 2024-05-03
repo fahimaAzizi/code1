2f0bafd87657df48da68e9bb903189d1c2374636
fahimaAzizi <118080917+fahimaAzizi@users.noreply.github.com>
2024-05-03 18:03:20 +0500

    Update py7.py

--- py7.py
+++ py7.py
@@ -11,14 +11,14 @@ def make_allowed_move(rod1, rod2):
     if not rods[rod2]:
         forward = True
     elif rods[rod1] and rods[rod1][-1] < rods[rod2][-1]:
-        forward = True              
+        forward = True
+                      
     if forward:
         print(f'Moving disk {rods[rod1][-1]} from {rod1} to {rod2}')
         rods[rod2].append(rods[rod1].pop())
     else:
         print(f'Moving disk {rods[rod2][-1]} from {rod2} to {rod1}')
         rods[rod1].append(rods[rod2].pop())
-    
     # display our progress
     print(rods)
 
@@ -29,11 +29,13 @@ def move(n, source, auxiliary, target):
         remainder = (i + 1) % 3
         if remainder == 1:
             print(f'Move {i + 1} allowed between {source} and {target}')
-            
+            make_allowed_move(source, target)
         elif remainder == 2:
             print(f'Move {i + 1} allowed between {source} and {auxiliary}')
+            make_allowed_move(source, auxiliary)
         elif remainder == 0:
-            print(f'Move {i + 1} allowed between {auxiliary} and {target}')            
+            print(f'Move {i + 1} allowed between {auxiliary} and {target}')
+            make_allowed_move(auxiliary, target)
 
 # initiate call from source A to target C with auxiliary B
 move(NUMBER_OF_DISKS, 'A', 'B', 'C')
